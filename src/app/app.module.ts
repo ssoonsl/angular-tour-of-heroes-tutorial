d093b20f8a28d9e30e70057c76ab74f7888246b1
ssoonsl <ssoonsl616+gh@gmail.com>
2017-11-25 17:07:21 +0800

    Inject MessageService into HeroService to display messages when heroes are loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
